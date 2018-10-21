76ade969dff18c4a5528741667b63c1f9fb218e4
Gery Akbar Fauzi <43455599+geryakbarf@users.noreply.github.com>
2018-10-21 10:26:35 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117049/latihan42/tabungan/Tabungan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117049.latihan42.tabungan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gery Akbar Fauzi
+ * Nama : Gery Akbar Fauzi 
+ * Kelas : PBO2 
+ * Nim : 10117049
+ * Deskripsi Program : Program ini berisi program untuk menghitung sisa tabungan
+ * setelah penarikan uang
+ */
+public class Tabungan {
+    private final int saldo;
+    
+    public Tabungan (int saldo){
+        this.saldo = saldo;
+        
+    }
+    public int ambilUang(int jumlah){
+        return saldo-jumlah;
+    }
+    
+}
